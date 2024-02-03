956042f79c2658af806eea255235a3efd78187e4
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-02-03 12:20:30 -0500

    frontend complete

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import { useLoaderData } from "react-router-dom";
+
+const About = () => {
+    return (
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Projects.js
@@ -0,0 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+
+const projects = useLoaderData()
+
+const Projects = () => {
+    return projects.map((project) => (
+        <div>
+          <h1>{project.name}</h1>
+          <img src={project.image} />
+          <a href={project.git}>
+            <button>Github</button>
+          </a>
+          <a href={project.live}>
+            <button>live site</button>
+          </a>
+        </div>
+      ));
+  }
+
+export default Projects
